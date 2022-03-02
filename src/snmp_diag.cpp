cba9e7652b9b32c504c17de284b83710a08a8e83
Roland Arsenault <roland@ccom.unh.edu>
2022-03-02 17:07:43 -0500

    Add list of oids as param

--- src/snmp_diag.cpp
+++ src/snmp_diag.cpp
@@ -6,49 +6,80 @@
 
 struct snmp_session session, *ss;
 std::string host = "localhost";
+std::vector<std::string> oids;
 ros::Publisher diag_pub;
 
 void timerCallback(const ros::TimerEvent event)
 {
-  struct snmp_pdu *pdu;
-  struct snmp_pdu *response;
-  pdu = snmp_pdu_create(SNMP_MSG_GET);
-
-  oid anOID[MAX_OID_LEN];
-  size_t anOID_len = MAX_OID_LEN;
-
-  get_node("ifInOctets.8", anOID, &anOID_len);
-  snmp_add_null_var(pdu, anOID, anOID_len);
 
   diagnostic_msgs::DiagnosticArray da;
+  da.header.stamp = ros::Time::now();
   diagnostic_msgs::DiagnosticStatus ds;
+
   ds.name = "snmp";
   ds.hardware_id = host;
   ds.level = diagnostic_msgs::DiagnosticStatus::WARN;
-  
-  int status = snmp_synch_response(ss, pdu, &response);
-  if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR)
-  {
-    diagnostic_msgs::KeyValue kv;
 
-    for(variable_list* vars = response->variables; vars; vars = vars->next_variable)
+  for(auto oid_str: oids)
+  {
+    snmp_pdu *pdu = snmp_pdu_create(SNMP_MSG_GET);
+    oid anOID[MAX_OID_LEN];
+    size_t anOID_len = MAX_OID_LEN;
+
+    get_node(oid_str.c_str(), anOID, &anOID_len);
+    snmp_add_null_var(pdu, anOID, anOID_len);
+
+    struct snmp_pdu *response = nullptr;
+    int status = snmp_synch_response(ss, pdu, &response);
+    if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR)
     {
-      print_variable(vars->name, vars->name_length, vars);
-      if(vars->type == ASN_COUNTER)
+
+      for(variable_list* vars = response->variables; vars; vars = vars->next_variable)
       {
-        kv.key = "ifInOctets.8";
-        kv.value = std::to_string(*vars->val.integer);
+        print_variable(vars->name, vars->name_length, vars);
+        diagnostic_msgs::KeyValue kv;
+        kv.key = oid_str;
+        switch(vars->type)
+        {
+          case ASN_INTEGER:
+          case ASN_COUNTER:
+            kv.value = std::to_string(*vars->val.integer);
+            ds.values.push_back(kv);
+            ds.level = diagnostic_msgs::DiagnosticStatus::OK;
+            break;
+          case ASN_COUNTER64:
+          case ASN_INTEGER64:
+            // kv.value = std::to_string(vars->val.counter64->high);
+            // ds.values.push_back(kv);
+            // ds.level = diagnostic_msgs::DiagnosticStatus::OK;
+            break;
+          case ASN_FLOAT:
+            kv.value = std::to_string(*vars->val.floatVal);
+            ds.values.push_back(kv);
+            ds.level = diagnostic_msgs::DiagnosticStatus::OK;
+            break;
+          case ASN_DOUBLE:
+            kv.value = std::to_string(*vars->val.doubleVal);
+            ds.values.push_back(kv);
+            ds.level = diagnostic_msgs::DiagnosticStatus::OK;
+            break;
+          default:
+            kv.value = reinterpret_cast<char *>(vars->val.string);
+            ds.values.push_back(kv);
+            ds.level = diagnostic_msgs::DiagnosticStatus::OK;
+            break;
+        }
         ds.values.push_back(kv);
-        ds.level = diagnostic_msgs::DiagnosticStatus::OK;
       }
     }
+    if (response)
+      snmp_free_pdu(response);
+
 
   }
   da.status.push_back(ds);
   diag_pub.publish(da);
 
-  if (response)
-    snmp_free_pdu(response);
 
 }
 
@@ -57,6 +88,8 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "snmp_diag");
 
   host = ros::param::param<std::string>("~host", host);
+  oids = ros::param::param("~oids", oids);
+  std::cerr << oids.size() << " oids" << std::endl;
 
   init_snmp("snmp_diag");
   snmp_sess_init( &session ); 
